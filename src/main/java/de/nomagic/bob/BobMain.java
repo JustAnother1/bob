f464ab0d85ddac80a21debfe8a0c71204d728efe
Lars Pötter <lars@ing-poetter.de>
2024-07-04 13:58:44 +0200

    Initial commit

---
+++ src/main/java/de/nomagic/bob/BobMain.java
@@ -0,0 +1,17 @@
+package de.nomagic.bob;
+
+public class BobMain
+{
+
+    public BobMain()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World !");
+    }
+
+}
